ea98a1c44151016de93ec8076a837909367f7a49
bitbucket_bai <sssswakaka@gmail.com>
2021-03-03 16:51:08 +0800

    冒泡排序

---
+++ BubbleSort.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class BubbleSort {
+
+    /**
+     * 冒泡排序
+     * 找最大，放到数组最后
+     * 比较相邻的两个元素，把较大的放在后面
+     * @param arr
+     */
+    public void sort(int[] arr){
+        int len = arr.length;
+        for (int i = len-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
