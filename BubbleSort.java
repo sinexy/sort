5018463cf7eca80b81e1a66946f3de63051a5ecf
bitbucket_bai <sssswakaka@gmail.com>
2021-03-04 13:22:55 +0800

    冒泡排序

--- BubbleSort.java
+++ BubbleSort.java
@@ -10,8 +10,8 @@ public class BubbleSort {
      */
     public void sort(int[] arr){
         int len = arr.length;
-        for (int i = len-1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 0; i < len-1; i++) {
+            for (int j = 0 ; j < len - i - 1; j++) {
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
